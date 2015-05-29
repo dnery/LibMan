30afce2ec6f6a4cb7f72c0127974df184f9a2eea
Danilo <dznery@gmail.com>
2015-05-29 20:58:32 -0300

    Missing 'list()' command and database handlers...

--- src/br/usp/icmc/ssc0103/Main.java
+++ src/br/usp/icmc/ssc0103/Main.java
@@ -1,33 +1,5 @@
 package br.usp.icmc.ssc0103;
 
-class Test{
-    private String string;
-    private boolean bool;
-    private int integer1;
-
-    public Test(String string, boolean bool, int integer1)
-    {
-        this.string = string;
-        this.bool = bool;
-        this.integer1 = integer1;
-    }
-
-    public String getString()
-    {
-        return string;
-    }
-
-    public boolean isBool()
-    {
-        return bool;
-    }
-
-    public int getInteger1()
-    {
-        return integer1;
-    }
-}
-
 public class Main
 {
     static Shell shell;
@@ -38,4 +10,3 @@ public class Main
         //shell.runCommand();
     }
 }
-
