0d4abe2c08a0444cda75be33881ebce2b48b4359
Danilo <dznery@gmail.com>
2015-05-29 16:51:38 -0300

    Added includes, exception classes...

--- src/br/usp/icmc/ssc0103/Database.java
+++ src/br/usp/icmc/ssc0103/Database.java
@@ -1,74 +1,101 @@
 package br.usp.icmc.ssc0103;
 
-<<<<<<< HEAD
-class User{
->>>>>>> 5a3cf9f2d565eeb3dfcc6b31aec8a7f53f34681b
+import java.util.Date;
+import java.util.List;
+
+class User
+{
 
 }
 
-class Book{
+class Book
+{
 
 }
 
-class Loan{
-
-	to string 
-	to csv
+class Loan
+{
+    //tostring
+    //tocsv
 }
 
+class DatabaseException extends Exception
+{
+    public DatabaseException() { super(); }
 
+    public DatabaseException(String message) { super(message); }
+}
 
-public class Database extends  {
+class AccessException extends Exception
+{
+    public AccessException() { super(); }
 
-	List<User> users;
-	List<Book> books;
-	List<Loan> loans;
-	
-	String userfile;
-	String bookfile;
-	String loanfile;
-	
-	
-	
-	public Detabase(String userfile, String bookfile, String loanfile){
-		// se nao exister cria os arquivos;
-		// carregar as listas
-		// seta os this.files
-		// 
-	}
-	private Serialize(String filename, String data){
-		// atualiza os arquivos
-	}
-	public void UserAdd(String username, int usertype) {
-	//add na lista
-	//atualiaza o csv File, serializa(this.userfile, user.toString)
-	}
-	public void CatalogAdd(String bookname, int booktype){
-	//igual o de cima	
-	}
-	
-	public void CheckIn(String bookname, Date date) throws DatabseException { 
-		
-		//se nao tiver emprestrado
-		// 
-	}
-	
-	public void CheckOut(String booknmae, String username, Date date,) throws
-							DatabseExpection, AccessExpection, AvailabilityExpection {
-	}
-	
-	
-	public void List(String... names) {
-		//local method list
-		for (String name : names) {
-			if (name.equals("users")){
-				//list user 
-			}
-			//bla bla
-		}
-	}
-	
-	
-	
-	
+    public AccessException(String message) { super(message); }
+}
+
+class AvailException extends Exception
+{
+    public AvailException() { super(); }
+
+    public AvailException(String message) { super(message); }
+}
+
+public class Database
+{
+    List<User> users;
+    List<Book> books;
+    List<Loan> loans;
+
+    String userFileName;
+    String bookFileName;
+    String loanFileName;
+
+    public Database(String userfile, String bookfile, String loanfile)
+    {
+        // se nao exister cria os arquivos;
+        // carregar as listas
+        // seta os this.files
+        //
+    }
+
+    public void UserAdd(String username, int usertype)
+    {
+        //add na lista
+        //atualiaza o csv File, serializa(this.userfile, user.toString)
+    }
+
+    public void CatalogAdd(String bookname, int booktype)
+    {
+        //igual o de cima
+    }
+
+    public void CheckIn(String bookname, Date date) throws DatabaseException
+    {
+
+        //se nao tiver emprestrado
+        //
+    }
+
+    public void CheckOut(String username, String bookname, Date date) throws
+                                                                      DatabaseException,
+                                                                      AccessException,
+                                                                      AvailException
+    {
+    }
+
+    public void List(String... names)
+    {
+        //local method list
+        for (String name : names) {
+            if (name.equals("users")) {
+                //list user
+            }
+        }
+    }
+
+    private void Serialize(String filename, String data)
+    {
+        //atualiza os arquivos
+    }
+
 }
