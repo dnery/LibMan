e3ccbde4fb812c3cfc75c794e3c572d60d9ef08d
Danilo <dznery@gmail.com>
2015-05-31 20:45:18 -0300

    Holy shit bro...

--- src/br/usp/icmc/ssc0103/Formatter.java
+++ src/br/usp/icmc/ssc0103/Formatter.java
@@ -2,9 +2,14 @@ package br.usp.icmc.ssc0103;
 
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 public class Formatter
 {
+    public static void outputUsers(Stream<User> users){
+
+    }
+
     public static long getDateDiff(Date date1, Date date2, TimeUnit timeUnit)
     {
         long diffInMillis = date2.getTime() - date1.getTime();
