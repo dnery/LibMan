a27292122108a34954e8d7b7ef38da7fee6d70db
Danilo <dznery@gmail.com>
2015-05-31 06:46:01 -0300

    All commands are 100% responsive

--- src/br/usp/icmc/ssc0103/Database.java
+++ src/br/usp/icmc/ssc0103/Database.java
@@ -2,118 +2,169 @@ package br.usp.icmc.ssc0103;
 
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Stream;
+
+enum UserType
+{
+    TUTOR, STUDENT, COMMUNITY
+}
+
+enum BookType
+{
+    TEXT, GENERAL
+}
 
 class User
 {
+    private String  name;
+    private int     maxBooks;
+    private int     curBooks;
+    private boolean academic;
+    private long    loanDuration;
+    private Date    suspendedTill;
 
-    String name;
-    int maxBooks;
-    int currentBooks;
-    boolean academic;
-    long loanDuration;
-    Date suspendedTill;
-
-    public User(String name,
-                int maxBooks,
-                int currentBooks,
-                boolean academic,
-                long loanDuration,
-                Date suspendedTill)
+    User(String name,
+         int maxBooks,
+         int curBooks,
+         boolean academic,
+         long loanDuration,
+         Date suspendedTill)
     {
         this.name = name;
         this.maxBooks = maxBooks;
-        this.currentBooks = currentBooks;
+        this.curBooks = curBooks;
         this.academic = academic;
         this.loanDuration = loanDuration;
         this.suspendedTill = suspendedTill;
     }
 
-    public String getName()
-    {
-        return name;
-    }
+    public String getName() { return name; }
 
-    public int getMaxBooks()
-    {
-        return maxBooks;
-    }
+    public int getMaxBooks() { return maxBooks; }
 
-    public int getCurrentBooks()
-    {
-        return currentBooks;
-    }
+    public int getCurBooks() { return curBooks; }
 
-    public boolean isAcademic()
-    {
-        return academic;
-    }
+    public boolean isAcademic() { return academic; }
 
-    public long getLoanDuration()
-    {
-        return loanDuration;
-    }
+    public long getLoanDuration() { return loanDuration; }
 
-    public Date getSuspendedTill()
-    {
-        return suspendedTill;
-    }
+    public Date getSuspendedTill() { return suspendedTill; }
 
-    public void setCurrentBooks(int currentBooks)
-    {
-        this.currentBooks = currentBooks;
-    }
+    public void setCurBooks(int curBooks) { this.curBooks = curBooks; }
 
-    public void setSuspendedTill(Date suspendedTill)
+    public void setSuspendedTill(Date suspendedTill) { this.suspendedTill = suspendedTill; }
+
+    public String serialize()
     {
-        this.suspendedTill = suspendedTill;
+        return this.name + "," +
+               this.maxBooks + "," +
+               this.curBooks + "," +
+               this.academic + "," +
+               this.loanDuration + "," +
+               this.suspendedTill.getTime();
     }
 }
 
 class Tutor extends User
 {
+    //When creating...
     public Tutor(String name) { super(name, 6, 0, true, (long) 5.184e+9, new Date()); }
 
-    public Tutor(String name, int currentBooks, long date)
+    //When recovering...
+    public Tutor(String name, int curBooks, long suspendedTill)
     {
-        super(name, 6, currentBooks, true, (long) 5.184e+9, new Date(date));
+        super(name, 6, curBooks, true, (long) 5.184e+9, new Date(suspendedTill));
     }
 }
 
 class Student extends User
 {
-
+    //When creating...
     public Student(String name) { super(name, 4, 0, true, (long) 1.296e+9, new Date()); }
 
-    public Student(String name, int currentBooks, long date)
+    //When recovering...
+    public Student(String name, int curBooks, long suspendedTill)
     {
-        super(name, 4, currentBooks, true, (long) 1.296e+9, new Date(date));
+        super(name, 4, curBooks, true, (long) 1.296e+9, new Date(suspendedTill));
     }
 }
 
 class Comunity extends User
 {
-
+    //When creating...
     public Comunity(String name) { super(name, 4, 0, false, (long) 1.296e+9, new Date()); }
 
-    public Comunity(String name, int currentBooks, long date)
+    //When recovering...
+    public Comunity(String name, int curBooks, long suspendedTill)
     {
-        super(name, 4, currentBooks, false, (long) 1.296e+9, new Date(date));
+        super(name, 4, curBooks, false, (long) 1.296e+9, new Date(suspendedTill));
     }
-
-
 }
 
 class Book
 {
+    private String  name;
+    private boolean type;
+    private boolean avail;
 
+    public Book(String name, boolean type, boolean avail)
+    {
+        this.name = name;
+        this.type = type;
+        this.avail = avail;
+    }
+
+    public String getName() { return name; }
+
+    public boolean isType() { return type; }
+
+    public boolean isAvail() { return avail; }
+
+    public void setAvail(boolean avail) { this.avail = avail; }
+
+    public String serialize() { return name + "," + type + "," + avail; }
 }
 
 class Loan
 {
-    //tostring
-    //tocsv
+    private int  userID;
+    private int  bookID;
+    private Date checkOutDate;
+    private Date checkInDate;
+    private Date realCID;
+
+    public Loan(int userID, int bookID, Date checkOutDate, Date checkInDate, Date realCID)
+    {
+        this.userID = userID;
+        this.bookID = bookID;
+        this.checkOutDate = checkOutDate;
+        this.checkInDate = checkInDate;
+        this.realCID = realCID;
+    }
+
+    public int getUserID() { return userID; }
+
+    public int getBookID() { return bookID; }
+
+    public Date getCheckOutDate() { return checkOutDate; }
+
+    public Date getCheckInDate() { return checkInDate; }
+
+    public Date getRealCID() { return realCID; }
+
+    public void setRealCID(Date realCID) { this.realCID = realCID; }
+
+    public String serialize()
+    {
+        return userID + "," +
+               bookID + "," +
+               checkOutDate.getTime() + "," +
+               checkInDate.getTime() + "," +
+               realCID.getTime();
+    }
 }
 
+// Database level attribute validity
 class DatabaseException extends Exception
 {
     public DatabaseException() { super(); }
@@ -121,6 +172,7 @@ class DatabaseException extends Exception
     public DatabaseException(String message) { super(message); }
 }
 
+// Interface level client clearance
 class AccessException extends Exception
 {
     public AccessException() { super(); }
@@ -128,6 +180,7 @@ class AccessException extends Exception
     public AccessException(String message) { super(message); }
 }
 
+// Interface level book availability
 class AvailException extends Exception
 {
     public AvailException() { super(); }
@@ -137,60 +190,119 @@ class AvailException extends Exception
 
 public class Database
 {
-    List<User> users;
-    List<Book> books;
-    List<Loan> loans;
+    private List<User> users;
+    private List<Book> books;
+    private List<Loan> loans;
+    private String     userFileName;
+    private String     bookFileName;
+    private String     loanFileName;
 
-    String userFileName;
-    String bookFileName;
-    String loanFileName;
+    // Static initializer is always run before class is used by any thread
+    private static final Database database = new Database("users.csv", "books.csv", "loans.csv");
 
-    public Database(String userfile, String bookfile, String loanfile)
+    // Private constructor in according to Singleton standards
+    private Database(String userFileName, String bookFileName, String loanFileName)
     {
-        // se nao exister cria os arquivos;
-        // carregar as listas
-        // seta os this.files
-        //
+        this.userFileName = userFileName;
+        this.bookFileName = bookFileName;
+        this.loanFileName = loanFileName;
+        // Se não existirem os arquivos, crie-os
+        // Carrega/cria listas condicionalmente
     }
 
-    public void UserAdd(String username, int usertype)
+    // The only existing instance is obtained always
+    public static Database getInstance() { return database; }
+
+    public void userAdd(String username, UserType usertype)
     {
-        //add na lista
-        //atualiaza o csv File, serializa(this.userfile, user.toString)
+        // TODO!!!
+        // Adiciona novo elemento usuario na lista users
+        // Atualiza o arquivo users.csv com o metodo append()
+
+        //DEBUG
+        if (usertype == UserType.TUTOR)
+            System.out.println("Tutor " + username + " has been added!");
+        else if (usertype == UserType.STUDENT)
+            System.out.println("Student " + username + " has been added!");
+        else if (usertype == UserType.COMMUNITY)
+            System.out.println("Community member " + username + " has been added!");
     }
 
-    public void CatalogAdd(String bookname, int booktype)
+    public void catalogAdd(String bookname, BookType booktype)
     {
-        //igual o de cima
+        // TODO!!!
+        // Semelhante ao userAdd(), porem com lista books e arquivo books.csv
+
+        //DEBUG
+        if (booktype == BookType.TEXT)
+            System.out.println("Textbook " + bookname + " has been catalogued!");
+        else if (booktype == BookType.GENERAL)
+            System.out.println("Literary book " + bookname + " has been catalogued!");
     }
 
-    public void CheckIn(String bookname, Date date) throws DatabaseException
-    {
-
-        //se nao tiver emprestrado
-        //
-    }
-
-    public void CheckOut(String username, String bookname, Date date) throws
+    public void checkOut(String username, String bookname, Date date) throws
                                                                       DatabaseException,
                                                                       AccessException,
                                                                       AvailException
     {
+        // TODO!!!
+        // Checa pela existencia do usuario na database
+        // Checa pela existencia do livro na database
+        // Checa se o usuario tem acesso ao livro
+        // Checa se o livro ja nao esta alugado
+        // Adiciona novo elemento na lista loans
+        // Atualiza o arquivo loans.csv com append()
+
+        //DEBUG
+        System.out.println(date.toString() + ": loaning \"" + bookname + "\" to " + username + ".");
     }
 
-    public void List(String... names)
+    public void checkIn(String bookname, Date date) throws DatabaseException,
+                                                           AvailException
     {
-        //local method list
-        for (String name : names) {
-            if (name.equals("users")) {
-                //list user
-            }
-        }
+        // TODO!!!
+        // Checa pela existencia do livro na database
+        // Checa se o livro ja nao esta disponivel
+        // MODIFICA o elemento certo na lista loans
+        // MODIFICA a linha certa no arquivo loans.csv com snipe()
+        // Ou simplesmente o reescreve inteiro com o metodo dump()
+
+        //DEBUG
+        System.out.println(date.toString() + ": \"" + bookname + "\" has been returned!");
     }
 
-    private void Serialize(String filename, String data)
+    private void append(String filename, String data)
     {
-        //atualiza os arquivos
+        // TODO!!!
+        // Adiciona a string "data" recebida de um
+        // serialize() no arquivo <filename>.csv
     }
 
+    // Esse metodo e opcional (vide snipe())
+    private void dump(String filename, String[] data)
+    {
+        // TODO!!!
+        // Se implementado, esse metodo deve
+        // reescrever o .csv inteiro usando
+        // o array de strings "data" recebido
+    }
+
+    // Esse metodo e opcional (vide dump())
+    private void snipe(String filename, String replaceable, String replacer)
+    {
+        // TODO!!!
+        // Se implementado, esse metodo deve
+        // reescrever uma string "replaceable"
+        // com uma string "replacer" recebida
+        // http://stackoverflow.com/questions/4505818/in-java-how-do-i-edit-1-line-of-a-text-file
+    }
+
+    // Recover users stream rather than list itself
+    public Stream<User> getUserStream() { return users.stream(); }
+
+    // Recover books stream rather than list itself
+    public Stream<Book> getBookStream() { return books.stream(); }
+
+    // Recover loans stream rather than list itself
+    public Stream<Loan> getLoanStream() { return loans.stream(); }
 }
--- src/br/usp/icmc/ssc0103/Shell.java
+++ src/br/usp/icmc/ssc0103/Shell.java
@@ -1,35 +1,19 @@
 package br.usp.icmc.ssc0103;
 
+import java.util.Date;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-enum UserType
-{
-    TUTOR, STUDENT, COMMUNITY
-}
-
-enum BookType
-{
-    TEXT, GENERAL
-}
-
 public class Shell
 {
-    enum Command
+    private enum Command
     {
-        USERADD, CATALOGADD,
-        CHECKOUT, CHECKIN,
-        LIST, EXIT, NOOP
+        NOOP, USERADD, CATALOGADD, CHECKOUT, CHECKIN, LIST, EXIT
     }
 
+    private Command command = Command.NOOP;
     private String line;
-    private Command command;
-
-    private Pattern pattern;
-    private Matcher matcher;
-
-    Shell() { command = Command.NOOP; }
 
     public void runCommand()
     {
@@ -38,25 +22,33 @@ public class Shell
         while (command != Command.EXIT) {
             System.out.print("> ");
 
-            if (userInput.hasNextLine())
+            if (userInput.hasNextLine()) {
                 line = userInput.nextLine();
 
-            if (checkCommand())
-                triggerCommand();
+                if (checkCommand())
+                    triggerCommand();
+            }
         }
     }
 
+    // No capture groups when simply validating
     public boolean checkCommand()
     {
         if (line.matches("^(.*[^\\\\];)$")) {
 
-            if (line.matches("^\\s*catalog\\s+add\\s+(.*[^\\\\,])\\s*;\\s*$")) {
-                command = Command.CATALOGADD;
-                System.out.println("catalogaddcmd premise...");
-            } else if (line.matches("^\\s*user\\s+add\\s+(.*[^\\\\,])\\s*;\\s*$")) {
+            if (line.matches("^\\s*user\\s+add\\s+(?:[^\\\\,]*);\\s*$")) {
                 System.out.println("useraddcmd premise...");
                 command = Command.USERADD;
-            } else if (line.matches("^\\s*list\\s+(.*[^\\\\,])\\s*;\\s*$")) {
+            } else if (line.matches("^\\s*catalog\\s+add\\s+(?:[^\\\\,]*);\\s*$")) {
+                command = Command.CATALOGADD;
+                System.out.println("catalogaddcmd premise...");
+            } else if (line.matches("^(?:[^\\\\,]*)checkout(?:[^\\\\,]*);\\s*$")) {
+                System.out.println("checkoutcmd premise...");
+                command = Command.CHECKOUT;
+            } else if (line.matches("^\\s*checkin\\s+(?:[^\\\\,]*);\\s*$")) {
+                System.out.println("checkin premise...");
+                command = Command.CHECKIN;
+            } else if (line.matches("^\\s*list\\s+(?:[^\\\\,]*);\\s*$")) {
                 System.out.println("listcmd premise...");
                 command = Command.LIST;
             } else if (line.matches("^\\s*exit\\s*;\\s*$")) {
@@ -65,7 +57,7 @@ public class Shell
             }
             return true;
         } else {
-            System.out.println("> Invalid command...");
+            System.out.println("> Not a valid command...");
             command = Command.NOOP;
             return false;
         }
@@ -73,67 +65,101 @@ public class Shell
 
     public void triggerCommand()
     {
+        Pattern pattern;
+        Matcher matcher;
+
         switch (command) {
 
-            case CATALOGADD:
-                //https://regex101.com/r/cZ7lK1/5
-                pattern = Pattern.compile(
-                        "^(?i)\\s*catalog\\s+add\\s+\"\\s*([a-zA-Z][a-zA-Z\\s]+[a-zA-Z])" +
-                                "\\s*\"\\s*(text|general|)\\s*;\\s*$");
+            case USERADD:
+                //https://regex101.com/r/cZ7lK1/7
+                pattern = Pattern.compile("^(?i)\\s*user\\s+add\\s+\\\"\\s*" +
+                                          "([a-zA-Z][a-zA-Z\\s]+[a-zA-Z])\\s*\\\"(?:\\s+" +
+                                          "(tutor|student|community))?\\s*;\\s*$");
 
                 if ((matcher = pattern.matcher(line)).find()) {
-                    System.out.print("> Command: 'catalog add' Captures: '" + matcher.group(1) +
-                                             "'");
-                    if (!matcher.group(2).matches(""))
-                        System.out.print(" '" + matcher.group(2) + "'");
-                    System.out.print("\n");
-
-                    //MANIPULATE DATABASE
-                } else System.out.println("> Invalid syntax...");
-
+                    if (matcher.group(2) != null && matcher.group(2).equals("tutor"))
+                        Database.getInstance().userAdd(matcher.group(1), UserType.TUTOR);
+                    else if (matcher.group(2) != null && matcher.group(2).equals("student"))
+                        Database.getInstance().userAdd(matcher.group(1), UserType.STUDENT);
+                    else if (matcher.group(2) == null || matcher.group(2).equals("community"))
+                        Database.getInstance().userAdd(matcher.group(1), UserType.COMMUNITY);
+                } else
+                    System.out.println("> Invalid syntax...");
                 break;
 
-            case USERADD:
-                //https://regex101.com/r/cZ7lK1/6
-                pattern = Pattern.compile(
-                        "^(?i)\\s*user\\s+add\\s+\"\\s*([a-zA-Z][a-zA-Z\\s]+[a-zA-Z])\\s*\"\\s*" +
-                                "(tutor|student|community|)\\s*;\\s*$");
+            case CATALOGADD:
+                //https://regex101.com/r/nU9qD4/1
+                pattern = Pattern.compile("^(?i)\\s*catalog\\s+add\\s+\\\"\\s*" +
+                                          "([a-zA-Z][a-zA-Z\\s]+[a-zA-Z])\\s*\\\"(?:\\s+" +
+                                          "(text|general))?\\s*;\\s*$");
 
                 if ((matcher = pattern.matcher(line)).find()) {
-                    System.out.print("> Command: 'user add' Captures: '" + matcher.group(1) + "'");
-                    if (!matcher.group(2).matches(""))
-                        System.out.print(" '" + matcher.group(2) + "'");
-                    System.out.print("\n");
-
-                    //MANIPULATE DATABASE
-                } else System.out.println("> Invalid syntax...");
-
+                    if (matcher.group(2) != null && matcher.group(2).equals("text"))
+                        Database.getInstance().catalogAdd(matcher.group(1), BookType.TEXT);
+                    else if (matcher.group(2) == null || matcher.group(2).equals("general"))
+                        Database.getInstance().catalogAdd(matcher.group(1), BookType.GENERAL);
+                } else
+                    System.out.println("> Invalid syntax...");
                 break;
 
             case CHECKOUT:
-                break;
-            case CHECKIN:
-                break;
-            case LIST:
-                //https://regex101.com/r/qI7wF9/8
-                pattern = Pattern.compile("^(?i)\\s*list\\s+(users|books|loans)(?:\\s+(users|books|loans))?(?:\\s+(users|books|loans))?\\s*;\\s*$");
+                //https://regex101.com/r/lV3vI3/1
+                pattern = Pattern.compile("^(?i)\\s*user\\s+\\\"\\s*" +
+                                          "([a-zA-Z][a-zA-Z\\s]+[a-zA-Z])" +
+                                          "\\s*\\\"\\s+checkout\\s+\\\"\\s*" +
+                                          "([a-zA-Z][a-zA-Z\\s]+[a-zA-Z])\\s*\\\"\\s*;\\s*$");
 
                 if ((matcher = pattern.matcher(line)).find()) {
-                    System.out.print("> Command: 'list' Captures: '" + matcher.group(1) + "'");
-                    if (matcher.group(2)!=null)
-                        System.out.print(" '" + matcher.group(2) + "'");
-                    if (matcher.group(3)!=null)
-                        System.out.print(" '" + matcher.group(3) + "'");
-                    System.out.print("\n");
-
-                    //MANIPULATE DATABASE
-                } else System.out.println("> Invalid syntax...");
-
+                    try {
+                        Database.getInstance().checkOut(matcher.group(1),
+                                                        matcher.group(2),
+                                                        new Date());
+                    } catch (DatabaseException e) {
+                        System.err.println("User or book does not exist!");
+                    } catch (AccessException e) {
+                        System.err.println("Loan is not authorized!");
+                    } catch (AvailException e) {
+                        System.err.println("Book already in use!");
+                    }
+                } else
+                    System.out.println("> Invalid syntax...");
                 break;
 
-            case EXIT:
+            case CHECKIN:
+                //https://regex101.com/r/rT4hC9/2
+                pattern = Pattern.compile("^(?i)\\s*checkin\\s+\\\"\\s*" +
+                                          "([a-zA-Z][a-zA-Z\\s]+[a-zA-Z])\\s*\\\"\\s*;\\s*$");
+
+                if ((matcher = pattern.matcher(line)).find()) {
+                    try {
+                        Database.getInstance().checkIn(matcher.group(1), new Date());
+                    } catch (DatabaseException e) {
+                        System.err.println("Book certainly does not exist!");
+                    } catch (AvailException e) {
+                        System.err.println("Book is already available!");
+                    }
+                } else
+                    System.out.println("> Invalid syntax...");
                 break;
-            case NOOP:
+
+            case LIST:
+                //https://regex101.com/r/qI7wF9/10
+                if (line.matches("^(?i)\\s*list(?:\\s+(?:users|books|loans))+\\s*;\\s*$")) {
+                    pattern = Pattern.compile("(users|books|loans)");
+                    matcher = pattern.matcher(line);
+
+                    while (matcher.find()) {
+                        if (matcher.group(1).equals("users"))
+                            System.out.println("Listing registered users!");
+                        else if (matcher.group(1).equals("books"))
+                            System.out.println("Listing registered books!");
+                        else if (matcher.group(1).equals("loans"))
+                            System.out.println("Listing registered loans!");
+                    }
+                }
+                break;
+
+            default:
                 break;
         }
     }
