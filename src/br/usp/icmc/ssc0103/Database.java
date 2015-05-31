78e461e62c93a759de7aac578a312ae419eefa4b
cristianolacerda <cristianolacerda@172.26.229.65>
2015-05-31 17:54:54 -0300

    implemented some database methods and did some refactoring

--- src/br/usp/icmc/ssc0103/Database.java
+++ src/br/usp/icmc/ssc0103/Database.java
@@ -1,168 +1,10 @@
 package br.usp.icmc.ssc0103;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Stream;
-
-enum UserType
-{
-    TUTOR, STUDENT, COMMUNITY
-}
-
-enum BookType
-{
-    TEXT, GENERAL
-}
-
-class User
-{
-    private String  name;
-    private int     maxBooks;
-    private int     curBooks;
-    private boolean academic;
-    private long    loanDuration;
-    private Date    suspendedTill;
-
-    User(String name,
-         int maxBooks,
-         int curBooks,
-         boolean academic,
-         long loanDuration,
-         Date suspendedTill)
-    {
-        this.name = name;
-        this.maxBooks = maxBooks;
-        this.curBooks = curBooks;
-        this.academic = academic;
-        this.loanDuration = loanDuration;
-        this.suspendedTill = suspendedTill;
-    }
-
-    public String getName() { return name; }
-
-    public int getMaxBooks() { return maxBooks; }
-
-    public int getCurBooks() { return curBooks; }
-
-    public boolean isAcademic() { return academic; }
-
-    public long getLoanDuration() { return loanDuration; }
-
-    public Date getSuspendedTill() { return suspendedTill; }
-
-    public void setCurBooks(int curBooks) { this.curBooks = curBooks; }
-
-    public void setSuspendedTill(Date suspendedTill) { this.suspendedTill = suspendedTill; }
-
-    public String serialize()
-    {
-        return this.name + "," +
-               this.maxBooks + "," +
-               this.curBooks + "," +
-               this.academic + "," +
-               this.loanDuration + "," +
-               this.suspendedTill.getTime();
-    }
-}
-
-class Tutor extends User
-{
-    //When creating...
-    public Tutor(String name) { super(name, 6, 0, true, (long) 5.184e+9, new Date()); }
-
-    //When recovering...
-    public Tutor(String name, int curBooks, long suspendedTill)
-    {
-        super(name, 6, curBooks, true, (long) 5.184e+9, new Date(suspendedTill));
-    }
-}
-
-class Student extends User
-{
-    //When creating...
-    public Student(String name) { super(name, 4, 0, true, (long) 1.296e+9, new Date()); }
-
-    //When recovering...
-    public Student(String name, int curBooks, long suspendedTill)
-    {
-        super(name, 4, curBooks, true, (long) 1.296e+9, new Date(suspendedTill));
-    }
-}
-
-class Comunity extends User
-{
-    //When creating...
-    public Comunity(String name) { super(name, 4, 0, false, (long) 1.296e+9, new Date()); }
-
-    //When recovering...
-    public Comunity(String name, int curBooks, long suspendedTill)
-    {
-        super(name, 4, curBooks, false, (long) 1.296e+9, new Date(suspendedTill));
-    }
-}
-
-class Book
-{
-    private String  name;
-    private boolean type;
-    private boolean avail;
-
-    public Book(String name, boolean type, boolean avail)
-    {
-        this.name = name;
-        this.type = type;
-        this.avail = avail;
-    }
-
-    public String getName() { return name; }
-
-    public boolean isType() { return type; }
-
-    public boolean isAvail() { return avail; }
-
-    public void setAvail(boolean avail) { this.avail = avail; }
-
-    public String serialize() { return name + "," + type + "," + avail; }
-}
-
-class Loan
-{
-    private int  userID;
-    private int  bookID;
-    private Date checkOutDate;
-    private Date checkInDate;
-    private Date realCID;
-
-    public Loan(int userID, int bookID, Date checkOutDate, Date checkInDate, Date realCID)
-    {
-        this.userID = userID;
-        this.bookID = bookID;
-        this.checkOutDate = checkOutDate;
-        this.checkInDate = checkInDate;
-        this.realCID = realCID;
-    }
-
-    public int getUserID() { return userID; }
-
-    public int getBookID() { return bookID; }
-
-    public Date getCheckOutDate() { return checkOutDate; }
-
-    public Date getCheckInDate() { return checkInDate; }
-
-    public Date getRealCID() { return realCID; }
-
-    public void setRealCID(Date realCID) { this.realCID = realCID; }
-
-    public String serialize()
-    {
-        return userID + "," +
-               bookID + "," +
-               checkOutDate.getTime() + "," +
-               checkInDate.getTime() + "," +
-               realCID.getTime();
-    }
-}
 
 // Database level attribute validity
 class DatabaseException extends Exception
@@ -193,6 +35,7 @@ public class Database
     private List<User> users;
     private List<Book> books;
     private List<Loan> loans;
+    private DatabaseFileHandler helper;
     private String     userFileName;
     private String     bookFileName;
     private String     loanFileName;
@@ -201,15 +44,22 @@ public class Database
     private static final Database database = new Database("users.csv", "books.csv", "loans.csv");
 
     // Private constructor according to Singleton standards
-    private Database(String userFileName, String bookFileName, String loanFileName)
-    {
+    private Database(String userFileName, String bookFileName, String loanFileName) {
+
+        helper = new DatabaseFileHandler();
+        users = new ArrayList<User>();
+        books = new ArrayList<Book>();
+
+
         this.userFileName = userFileName;
         this.bookFileName = bookFileName;
         this.loanFileName = loanFileName;
-        // Se n�o existirem os arquivos, crie-os
-        // Carrega/cria listas condicionalmente
-    }
 
+        helper.loadUsers();
+        helper.loadBooks();
+        helper.loadLoans();
+
+    }
     // The only existing instance is obtained always
     public static Database getInstance() { return database; }
 
@@ -219,6 +69,9 @@ public class Database
         // TODO!!!
         // Adiciona novo elemento usuario na lista users
         // Atualiza o arquivo users.csv com o metodo append()
+        User user = new User(username, usertype);
+        users.add(user);
+        helper.append(userFileName, user.serialize());
 
         //DEBUG
         if (usertype == UserType.TUTOR)
@@ -275,39 +128,119 @@ public class Database
         System.out.println(date.toString() + ": \"" + bookname + "\" has been returned!");
     }
 
-    // File handlers: writer
-    private void append(String filename, String data)
-    {
-        // TODO!!!
-        // Adiciona a string "data" recebida de um
-        // serialize() no arquivo <filename>.csv
+    // File handlers:
+    class DatabaseFileHandler {
+        public void loadUsers() {
+            try {
+                BufferedReader in = new BufferedReader(new FileReader(userFileName));
+                String csv;
+                in.readLine(); // Pula a primeira linha (cabeÁalho)
+                while((csv = in.readLine()) != null) {
+                    users.add(new User(csv));
+                }
+            }
+            catch(FileNotFoundException e) {
+                try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                        new FileOutputStream(userFileName), "utf-8"))) {
+                    writer.write("Registred users:");
+                    writer.close();
+                }
+                catch (IOException er) {
+                    System.out.println("Error creating users file!");
+                }
+            }
+            catch(IOException e) {
+                System.out.println("Error reading Users file!");
+            }
+        }
+
+        public void loadBooks(){
+            try {
+                BufferedReader in = new BufferedReader(new FileReader(bookFileName));
+                String csv;
+                in.readLine(); // Pula a primeira linha (cabeÁalho)
+                while((csv = in.readLine()) != null) {
+                    books.add(new Book(csv));
+                }
+                in.close();
+            }
+            catch(FileNotFoundException e) {
+                try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                        new FileOutputStream(bookFileName), "utf-8"))) {
+                    writer.write("Registred books:");
+                    writer.close();
+                }
+                catch (IOException er) {
+                    System.out.println("Error creating books file!");
+                }
+            }
+            catch(IOException e) {
+                System.out.println("Error reading books file!");
+            }
+        }
+
+        public void loadLoans() {
+            try {
+                BufferedReader in = new BufferedReader(new FileReader(loanFileName));
+                String csv;
+                in.readLine(); // Pula a primeira linha (cabeÁalho)
+                while((csv = in.readLine()) != null) {
+                    loans.add(new Loan(csv));
+                }
+                in.close();
+            }
+            catch(FileNotFoundException e) {
+                try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                        new FileOutputStream(loanFileName), "utf-8"))) {
+                    writer.write("Registred Loans:");
+                    writer.close();
+                }
+                catch (IOException er) {
+                    System.out.println("Error creating loans file!");
+                }
+            }
+            catch(IOException e) {
+                System.out.println("Error reading loans file!");
+            }
+        }
+
+        private void append(String filename, String data)
+        {
+            try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(filename, true)))) {
+                out.println("\n" + data);
+            }catch (IOException e) {
+                System.err.println(e);
+            }
+        }
+
+        // Esse metodo e opcional (vide snipe())
+        private void dump(String filename, String[] data)
+        {
+            // TODO!!!
+            // Se implementado, esse metodo deve
+            // reescrever o .csv inteiro usando
+            // o array de strings "data" recebido
+        }
+
+        // Esse metodo e opcional (vide dump())
+        private void snipe(String filename, String replaceable, String replacer)
+        {
+            // TODO!!!
+            // Se implementado, esse metodo deve
+            // reescrever uma string "replaceable"
+            // com uma string "replacer" recebida
+            // http://stackoverflow.com/questions/4505818/in-java-how-do-i-edit-1-line-of-a-text-file
+        }
+
+        // Recover users stream rather than list itself
+        public Stream<User> getUserStream() { return users.stream(); }
+
+        // Recover books stream rather than list itself
+        public Stream<Book> getBookStream() { return books.stream(); }
+
+        // Recover loans stream rather than list itself
+        public Stream<Loan> getLoanStream() { return loans.stream(); }
     }
 
-    // Esse metodo e opcional (vide snipe())
-    private void dump(String filename, String[] data)
-    {
-        // TODO!!!
-        // Se implementado, esse metodo deve
-        // reescrever o .csv inteiro usando
-        // o array de strings "data" recebido
-    }
 
-    // Esse metodo e opcional (vide dump())
-    private void snipe(String filename, String replaceable, String replacer)
-    {
-        // TODO!!!
-        // Se implementado, esse metodo deve
-        // reescrever uma string "replaceable"
-        // com uma string "replacer" recebida
-        // http://stackoverflow.com/questions/4505818/in-java-how-do-i-edit-1-line-of-a-text-file
-    }
-
-    // Recover users stream rather than list itself
-    public Stream<User> getUserStream() { return users.stream(); }
-
-    // Recover books stream rather than list itself
-    public Stream<Book> getBookStream() { return books.stream(); }
-
-    // Recover loans stream rather than list itself
-    public Stream<Loan> getLoanStream() { return loans.stream(); }
 }
---
+++ src/br/usp/icmc/ssc0103/tables.java
@@ -0,0 +1,151 @@
+package br.usp.icmc.ssc0103;
+
+import java.util.Date;
+
+enum UserType
+{
+    COMMUNITY, STUDENT, TUTOR
+}
+
+enum BookType
+{
+    TEXT, GENERAL
+}
+
+class User
+{
+    private String   name;
+    private UserType type;
+    private int      curBooks;
+    private Date     suspendedTill;
+
+    User(String csv)  {
+
+        String[] splited = csv.split(",");
+
+        this.name = splited[0];
+        this.type.valueOf(splited[1]);
+        this.curBooks = Integer.parseInt(splited[2]);
+        this.suspendedTill = new Date(Long.parseLong(splited[3]));
+
+    }
+
+    User (String userName, UserType usertype) {
+        this.name = userName;
+        this.type = usertype;
+        this.curBooks = 0;
+        this.suspendedTill = new Date();
+
+    }
+
+    public String getName() { return name; }
+
+    public int getMaxBooks() {
+        if (type == UserType.COMMUNITY) {
+            return 2;
+        }
+        else if (type == UserType.STUDENT) {
+            return 4;
+        }
+        else return 6;
+    }
+
+    public int getCurBooks() { return curBooks; }
+
+    public boolean isAcademic()
+    {
+        if (type == UserType.COMMUNITY) {
+            return false;
+        }
+        else return true;
+    }
+
+    public long getLoanDuration() {
+        if (type == UserType.TUTOR) {
+            return (long) 5.184e+9;
+        }
+        else return (long) 1.296e+9;
+
+    }
+
+    public Date getSuspendedTill() { return suspendedTill; }
+
+    public void setCurBooks(int curBooks) { this.curBooks = curBooks; }
+
+    public void setSuspendedTill(Date suspendedTill) { this.suspendedTill = suspendedTill; }
+
+    public String serialize()
+    {
+        return this.name + "," +
+                this.type.toString() + "," +
+                this.curBooks + "," +
+                this.suspendedTill.getTime();
+    }
+}
+
+class Book
+{
+    private String  name;
+    private boolean type;
+    private boolean avail;
+
+    public Book(String csv)
+    {
+        String[] splited = csv.split(",");
+
+        this.name = splited[0];
+        this.type = Boolean.getBoolean(splited[1]);
+        this.avail = Boolean.getBoolean(splited[2]);
+    }
+
+    public String getName() { return name; }
+
+    public boolean isType() { return type; }
+
+    public boolean isAvail() { return avail; }
+
+    public void setAvail(boolean avail) { this.avail = avail; }
+
+    public String serialize() { return name + "," + type + "," + avail; }
+}
+
+class Loan
+{
+    private int  userID;
+    private int  bookID;
+    private Date checkOutDate;
+    private Date checkInDate;
+    private Date realCID;
+
+    public Loan(String csv)
+    {
+        String[] splited = csv.split(",");
+
+        this.userID = Integer.parseInt(splited[0]);
+        this.bookID = Integer.parseInt(splited[1]);
+        this.checkOutDate = new Date(Long.parseLong(splited[2]));
+        this.checkInDate =  new Date(Long.parseLong(splited[3]));
+        this.realCID = new Date(Long.parseLong(splited[4]));
+    }
+
+    public int getUserID() { return userID; }
+
+    public int getBookID() { return bookID; }
+
+    public Date getCheckOutDate() { return checkOutDate; }
+
+    public Date getCheckInDate() { return checkInDate; }
+
+    public Date getRealCID() { return realCID; }
+
+    public void setRealCID(Date realCID) { this.realCID = realCID; }
+
+    public String serialize()
+    {
+        return userID + "," +
+                bookID + "," +
+                checkOutDate.getTime() + "," +
+                checkInDate.getTime() + "," +
+                realCID.getTime();
+    }
+}
