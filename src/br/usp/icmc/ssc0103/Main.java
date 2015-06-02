a153e32fdbbcceae516b4aa94a3351a4ff2dca4f
mvjunq <codejunq@gmail.com>
2015-06-01 21:22:25 -0300

    Commenting and formatting README

--- src/br/usp/icmc/ssc0103/Main.java
+++ src/br/usp/icmc/ssc0103/Main.java
@@ -14,10 +14,7 @@ public class Main
     public static void main(String[] args)
     {
         try {
-            if (args.length != 1) {
-                Formatter.outputError("Missing the date argument, please see UTILIZACAO in the README.md, halting main routine..");
-                System.exit(1);
-            }
+
             shell = new Shell(args);
             shell.runCommand();
         } catch (ParseException e){
